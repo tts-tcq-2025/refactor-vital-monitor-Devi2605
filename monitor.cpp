3a1eb652691f74badb0a234c2c154e102c5036e3
Devi2605 <devibosch2605@gmail.com>
2025-07-29 10:51:23 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -57,17 +57,17 @@ void duplicate(){
 int vitalsOk(float temperature, float pulseRate, float spo2) {
   if (temperature > 102 || temperature < 95) {
  cout<<" Temperature is critical!\n";
- Alert();
+ duplicate();
  return 0;
 }
  else if (pulseRate < 60 || pulseRate > 100) {
          cout << "Pulse Rate is out of range!\n";
-        Alert();
+        duplicate();
         return 0;
  
  } else if (spo2 < 90) {
         cout << "Oxygen Saturation out of range!\n";
-        Alert();
+        duplicate();
         return 0;
     }
     return 1;
