8d5c2d8c24744720b3eee360fb0b594aa4aeff8c
Devi2605 <devibosch2605@gmail.com>
2025-07-29 10:41:51 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,3 +1,43 @@
+// #include "./monitor.h"
+// #include <assert.h>
+// #include <thread>
+// #include <chrono>
+// #include <iostream>
+// using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+
+// int vitalsOk(float temperature, float pulseRate, float spo2) {
+//   if (temperature > 102 || temperature < 95) {
+//     cout << "Temperature is critical!\n";
+//     for (int i = 0; i < 6; i++) {
+//       cout << "\r* " << flush;
+//       sleep_for(seconds(1));
+//       cout << "\r *" << flush;
+//       sleep_for(seconds(1));
+//     }
+//     return 0;
+//   } else if (pulseRate < 60 || pulseRate > 100) {
+//     cout << "Pulse Rate is out of range!\n";
+//     for (int i = 0; i < 6; i++) {
+//       cout << "\r* " << flush;
+//       sleep_for(seconds(1));
+//       cout << "\r *" << flush;
+//       sleep_for(seconds(1));
+//     }
+//     return 0;
+//   } else if (spo2 < 90) {
+//     cout << "Oxygen Saturation out of range!\n";
+//     for (int i = 0; i < 6; i++) {
+//       cout << "\r* " << flush;
+//       sleep_for(seconds(1));
+//       cout << "\r *" << flush;
+//       sleep_for(seconds(1));
+//     }
+//     return 0;
+//   }
+//   return 1;
+// }
+
+//////////////////////////////////////////////////////////////////////
 #include "./monitor.h"
 #include <assert.h>
 #include <thread>
@@ -5,34 +45,31 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+void Alert(){
+ for (int i = 0; i < 6; i++) {
+      cout << "\r* " << flush;
+      sleep_for(seconds(1));
+      cout << "\r *" << flush;
+      sleep_for(seconds(1));
+    }
+
+}
 int vitalsOk(float temperature, float pulseRate, float spo2) {
   if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+ cout<<" Temperature is critical!\n";
+ Alert();
+ return 0;
 }
+ else if (pulseRate < 60 || pulseRate > 100) {
+         cout << "Pulse Rate is out of range!\n";
+        Alert();
+        return 0;
+ 
+ } else if (spo2 < 90) {
+        cout << "Oxygen Saturation out of range!\n";
+        Alert();
+        return 0;
+    }
+    return 1;
+}
+
