0b2787ac39bbbba8063710e6fc585cf6e36b1e91
Devi2605 <devibosch2605@gmail.com>
2025-08-10 10:04:23 +0530

    Create duplicate_function.h

---
+++ duplicate_function.h
@@ -0,0 +1,15 @@
+#include "duplicate.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+
+void duplicate() {
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
